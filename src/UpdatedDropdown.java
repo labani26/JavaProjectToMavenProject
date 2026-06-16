d95bd6e4ea890def91dab6ebdeb60c45df18b111
Labani sardar <mail2labanisardar@gmail.com>
2026-06-16 05:10:47 -0700

    16/6/2026 commit

--- src/UpdatedDropdown.java
+++ src/UpdatedDropdown.java
@@ -3,39 +3,64 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class UpdatedDropdown {
 
-	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
-		
+    public static void main(String[] args) {
+
         WebDriver driver = new ChromeDriver();
-		
-        driver.get("https://www.spicejet.com/");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
-        
-        driver.findElement(By.xpath("//div[@class=\"css-1dbjc4n r-1awozwy r-18u37iz r-1wtj0ep\"]")).click();
-        
-        Thread.sleep(3000);
-        
-        int i = 1;
-        while(i<3) {
-        	driver.findElement(By.xpath("//div[@class=\"r-1awozwy r-19m6qjp r-y47klf r-1loqt21 r-eu3ka r-1777fci r-1otgn73 r-1i6wzkk r-lrvibr r-1aockid css-1dbjc4n\"]")).click();
-            i++;
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://www.spicejet.com/");
+
+        // Open Destination
+        driver.findElement(By.xpath("//*[@value=\"Select Destination\"]")).click();
+
+        // Select Ahmedabad
+        driver.findElement(By.xpath("//*[text()='Ahmedabad']")).click();
+
+        // Open Calendar
+//        wait.until(ExpectedConditions.elementToBeClickable(
+//                By.xpath("//*[contains(text(),'Select Date')]")))
+//                .click();
+//
+//        // Click visible date 20
+//        wait.until(ExpectedConditions.elementToBeClickable(
+//                By.xpath("(//*[@data-testid='undefined-calendar-day-20'])[last()]")))
+//                .click();
+
+        // Open Travellers
+        driver.findElement(By.xpath("//div[normalize-space()='1 Adult']")).click();
+
+        // Add 2 Adults
+        int i = 1; 
+        while(i<3) { 
+        	driver.findElement( By.cssSelector("div[data-testid='Adult-testID-plus-one-cta']") ).click(); 
+        i++;
         }
         
+        // Add 5 Children
         int j = 0;
         while(j<5){
-        	
-        	driver.findElement(By.cssSelector("path[fill-rule='nonzero']")).click();
-        	j++;
+        	driver.findElement(By.cssSelector("div[data-testid='Children-testID-plus-one-cta']")).click(); 
+        	// driver.findElement(By.xpath("//div[@data-testid='Children-testID-plus-one-cta']")).click(); 
+        	j++; 
         }
         
-//        for(i=0;i<5;i++) {
-//        	driver.findElement(By.cssSelector("path[fill-rule='nonzero']")).click();
-//        }
-        
-	}
+        // Add 4 Infants
+        for(int k = 0; k < 4 ; k++) { 
+        	driver.findElement(By.xpath("//*[@data-testid='Infant-testID-plus-one-cta']")).click();
+        	}
 
+        // Done
+        driver.findElement(By.cssSelector("[data-testid='home-page-travellers-done-cta']"))
+        .click();
+
+        // Search Flights
+       driver.findElement(By.cssSelector("[data-testid='home-page-flight-cta']")).click();
+    }
 }
