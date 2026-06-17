086019f12c98d5eb3bc6ef1fb42ed9eb2eaf7fea
Labani sardar <mail2labanisardar@gmail.com>
2026-06-17 03:53:25 -0700

    17/6/2026 commit msg

--- src/UpdatedDropdown.java
+++ src/UpdatedDropdown.java
@@ -3,12 +3,11 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class UpdatedDropdown {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -16,12 +15,18 @@ public class UpdatedDropdown {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         driver.get("https://www.spicejet.com/");
+        
+        driver.findElement(By.xpath("//div[text()='From']")).click();
+        
+     // Select Ahmedabad
+        driver.findElement(By.xpath("//*[text()='BLR']")).click();
+        
+        Thread.sleep(2000);
 
         // Open Destination
-        driver.findElement(By.xpath("//*[@value=\"Select Destination\"]")).click();
-
-        // Select Ahmedabad
-        driver.findElement(By.xpath("//*[text()='Ahmedabad']")).click();
+        driver.findElement(By.xpath("//div[normalize-space()='To']")).click(); 
+        
+        driver.findElement(By.xpath("//*[text()='MAA']")).click();
 
         // Open Calendar
 //        wait.until(ExpectedConditions.elementToBeClickable(
@@ -34,7 +39,9 @@ public class UpdatedDropdown {
 //                .click();
 
         // Open Travellers
-        driver.findElement(By.xpath("//div[normalize-space()='1 Adult']")).click();
+        driver.findElement(
+        	    By.xpath("//div[.//svg[@data-testid='svg-img']]")
+        	).click();
 
         // Add 2 Adults
         int i = 1; 
