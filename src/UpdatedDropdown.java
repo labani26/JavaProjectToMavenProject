3cf2275f1602b721b36284d60eeb2ddadff86cf0
Labani sardar <mail2labanisardar@gmail.com>
2026-06-18 04:24:40 -0700

    18/6/2026 1st commit

--- src/UpdatedDropdown.java
+++ src/UpdatedDropdown.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
+
 public class UpdatedDropdown {
 
     public static void main(String[] args) throws InterruptedException{
@@ -27,20 +28,22 @@ public class UpdatedDropdown {
         driver.findElement(By.xpath("//div[normalize-space()='To']")).click(); 
         
         driver.findElement(By.xpath("//*[text()='MAA']")).click();
+        
+        
+     // Click on the Senior Citizen radio button
+        driver.findElement(
+           By.xpath("(//*[local-name()='circle' and @cx='9' and @cy='9' and @r='8'])[2]")
+        ).click();
 
-        // Open Calendar
-//        wait.until(ExpectedConditions.elementToBeClickable(
-//                By.xpath("//*[contains(text(),'Select Date')]")))
-//                .click();
-//
-//        // Click visible date 20
-//        wait.until(ExpectedConditions.elementToBeClickable(
-//                By.xpath("(//*[@data-testid='undefined-calendar-day-20'])[last()]")))
-//                .click();
+        // Just print a confirmation
+        System.out.println("Senior Citizen option clicked successfully.");
+        	
+        //how many circle button are there
+        System.out.println(driver.findElement(By.xpath("//*[local-name()='circle' and @cx='9' and @cy='9' and @r='8']")).getSize());
 
         // Open Travellers
         driver.findElement(
-        	    By.xpath("//div[.//svg[@data-testid='svg-img']]")
+        	    By.xpath("//div[text()='Passengers']/following::svg[1]")
         	).click();
 
         // Add 2 Adults
@@ -71,3 +74,21 @@ public class UpdatedDropdown {
        driver.findElement(By.cssSelector("[data-testid='home-page-flight-cta']")).click();
     }
 }
+
+//Code Snippet download
+//
+//
+//WebDriver driver =new ChromeDriver();
+//
+//driver.get("https://rahulshettyacademy.com/dropdownsPractise/"); //URL in the browser
+//
+//Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+//
+////Assert.assertFalse(true);System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+//
+//driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+//
+//System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+//
+//Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+//
