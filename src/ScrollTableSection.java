b838ceb9ce2de0f3d650c9556d35f3e9857b60ae
Labani sardar <mail2labanisardar@gmail.com>
2026-07-04 07:35:02 -0500

    4/7/2026 2nd commit

--- src/ScrollTableSection.java
+++ src/ScrollTableSection.java
@@ -1,8 +1,12 @@
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class ScrollTableSection {
 
@@ -26,6 +30,36 @@ public class ScrollTableSection {
         //It is similar to Selenium: driver.findElement(By.cssSelector(".executeScript"));
         //querySelector('.tableFixHead') → Finds the first element with the class tableFixHead.
         //scrollTop = 500 → Scrolls that element 500 pixels downward.
-	}
+        
+        
+        List<WebElement> rowList = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+        
 
+        int sum = 0;
+
+        // Calculate the sum of the 4th column
+        for (int i = 0; i < rowList.size(); i++) {
+
+            int amount = Integer.parseInt(rowList.get(i).getText());
+            System.out.println(amount);
+
+            sum = sum + amount;
+        }
+
+        System.out.println("Calculated Total = " + sum);
+
+        // Get the displayed total
+        String visibleAmount = driver.findElement(By.cssSelector(".totalAmount"))
+                                     .getText()
+                                     .split(":")[1]
+                                     .trim();
+
+        int displayedTotal = Integer.parseInt(visibleAmount);
+
+        System.out.println("Displayed Total = " + displayedTotal);
+
+        // Compare both totals
+        Assert.assertEquals(sum, displayedTotal);
+        driver.quit();
+	}
 }
