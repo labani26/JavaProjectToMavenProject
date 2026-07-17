f536a152edf554a779e5a5dd1ee736a139e2594c
Labani sardar <mail2labanisardar@gmail.com>
2026-07-17 06:15:17 -0500

    17/7/2026 commit 1st

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+
+public class Demo {
+
+    public static void main(String[] args) {
+    	
+        Assert.assertEquals(5, 5);
+        System.out.println("Working");
+        
+    }
+}
---
+++ src/TestTestNg.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class TestTestNg {
+
+    @Test
+    public void testDropdown() throws InterruptedException {
+        // Move everything from your main() method here
+    }
+}
